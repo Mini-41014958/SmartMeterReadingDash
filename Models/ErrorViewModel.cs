06f916d46582f9b4306edf93e4767bae42538c13
Piyush Dubey <Piyush.dubey@reliancegroupindia.com>
2026-07-30 14:19:06 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SmartMeterReadingDash.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
